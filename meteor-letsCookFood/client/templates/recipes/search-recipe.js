252879fd68ac7640308d58b36b68c613e09700b8
Justin <justinwayneolson@gmail.com>
2016-04-02 17:32:05 -0400

    restructed, and added db call

--- meteor-letsCookFood/client/templates/recipes/search-recipe.js
+++ meteor-letsCookFood/client/templates/recipes/search-recipe.js
@@ -23,53 +23,62 @@ Template.recipes.rendered = function(){
          $("#query-parameters").append('<div value="' + $(this).attr("value") + '" class="row"><h4>' + $(this).text() + '</h4><input name="' + $(this).attr("id") + '" type="text"> ' + query_results[$(this).attr("value")].format + '</div>');
          }
       });
+      $("#add-favorite").click(function(){
+         var recipe = {
+            "name":$(this).attr("name"),
+            "id":$(this).attr("id")
+         };
+         Meteor.call('recipeListInsert', recipe, function(error, results){
+            return 0;
+         });
+      });
    });
-};
 
+}
+   function getRecipeJson() {
+      var apiKey = "pzNnTT0Geimshk4zTvFtsrAINqiHp1YEhgojsnngmYNQaIZoom";
+      var url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?" + endpoint + "&mashape-key=" + apiKey;
+      console.log(url);
+      $.ajax({
+         type: "GET",
+         dataType: 'json',
+         cache: false,
+         url: url,
+         success: function (data) {
+            console.log("penis");
+            console.log(data);
+            $.each(data.results, function(index, value)
+            {
+               recipe_id=value.id;
+               recipe_title=value.title;
+               recipe_title=recipe_title.replace(/\s+/g, '-').toLowerCase();
+               $("#recipe-results").append('<div class="container-fluid"><h3>' + value.title + '</h3> <img src="' + value.image + '"></img><button name="' + recipe_title + '" class="recipe-link btn btn-success" id="' + recipe_id + '">View Full Recipe</button><button class="btn btn-info" id="add-favorite">Add To Favorites</button></div>');
+            });
+            /*$(".recipe-link").click(function(){
+               recipe_information=$(this).attr("id");
+               var recipe_url="https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract?forceExtraction=false&url=http%3A%2F%2Fwww.melskitchencafe.com%2F" + recipe_endpoint + "%2F&mashape-key=" + apiKey;
+               console.log(recipe_url);
+               $.ajax({
+                  type: "GET",
+                  dataType: 'json',
+                  cache: false,
+                  url: recipe_url,
+                  success: function (data)
+                  {
+                     recipe_conent=data;
+                     console.log(data);
+                  }
+
+               });
+
+            });*/
+         }
+      });
+   }
 Template.recipes.events({
    'click #submit-query': function(e){
       endpoint=$("form").serialize();
-     function getRecipeJson() {
-         var apiKey = "pzNnTT0Geimshk4zTvFtsrAINqiHp1YEhgojsnngmYNQaIZoom";
-         var url = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/searchComplex?" + endpoint + "&mashape-key=" + apiKey;
-         console.log(url);
-         $.ajax({
-             type: "GET",
-             dataType: 'json',
-             cache: false,
-             url: url,
-             success: function (data) {
-               console.log("penis");
-               console.log(data);
-               $.each(data.results, function(index, value)
-               {
-                  recipe_title=value.title;
-                  recipe_title=recipe_title.replace(/\s+/g, '-').toLowerCase();
-                  $("#recipe-results").append('<div class="container-fluid"><h4">' + value.title + '<h4> <img src="' + value.image + '"></img><button class="recipe-link btn btn-success" id="' + recipe_title + '">View Full Recipe</button></div>');
-               });
-               $(".recipe-link").click(function(){
-                  recipe_details=$(this).attr("id");
-                  var recipe_url="https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/extract?forceExtraction=false&url=http%3A%2F%2Fwww.melskitchencafe.com%2F" + recipe_endpoint + "%2F&mashape-key=" + apiKey;
-                  console.log(recipe_url);
-                  $.ajax({
-                     type: "GET",
-                     dataType: 'json',
-                     cache: false,
-                     url: recipe_url,
-                     success: function (data)
-                     {
-                        recipe_conent=data;
-                        console.log(data);
-                     }
-
-               });
-
-         });
-     }
-   });
-}
-
       getRecipeJson();
-      }
+   }
 });
 
