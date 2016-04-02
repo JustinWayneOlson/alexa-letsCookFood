b903acc504233e5b43ea54474d4f5de7ec0c523e
Justin <justinwayneolson@gmail.com>
2016-04-02 02:37:50 -0400

    added big_oven api call

--- meteor-letsCookFood/client/templates/search-recipe/search-recipe.js
+++ meteor-letsCookFood/client/templates/search-recipe/search-recipe.js
@@ -1,20 +1,22 @@
-Template.searchRecipe.rendered = function() {
+my_data={};
+Template.searchRecipe.events({
+'click #search-button': function(e) {
+   var recipeKeyword = $("#recipe_search").val();
   function getRecipeJson() {
       var apiKey = "72wOanSqYlM4R6EgFAgS229UX0yx4fc9";
       var recipeId = 196149;
-      var url = "http://api.bigoven.com/recipe/" + recipeId + "?api_key=" + apiKey;
+      var url = "http://api.bigoven.com/recipes?pg=1&rpp=25&any_kw=" + recipeKeyword + "&api_key=" + apiKey;
       $.ajax({
           type: "GET",
           dataType: 'json',
           cache: false,
           url: url,
           success: function (data) {
-              console.log(data);
-              $("#recipeTitle").html(data.Title);
-              $("#instructions").html(data.Instructions);
+             console.log(data);
+             my_data=data;
           }
       });
   }
-
   getRecipeJson();
 }
+});
