e56d72c5355d3f8ad95703887f079ef951b23d36
Conner Simmering <connersimmering@gmail.com>
2016-04-02 01:06:29 -0600

    spigot

--- meteor-letsCookFood/client/templates/landing_page/landing_page.js
+++ meteor-letsCookFood/client/templates/landing_page/landing_page.js
@@ -1,7 +1,20 @@
-Template.landingPage.events({
-  'click [data-action="signUp"]': function(e) {
-    e.stopPropagation();
-    $('#login-dropdown-list').addClass('open');
-    $('#login-username-or-email').focus();
+Template.landingPage.rendered = function() {
+  function getRecipeJson() {
+      var apiKey = "72wOanSqYlM4R6EgFAgS229UX0yx4fc9";
+      var recipeId = 196149;
+      var url = "http://api.bigoven.com/recipe/" + recipeId + "?api_key=" + apiKey;
+      $.ajax({
+          type: "GET",
+          dataType: 'json',
+          cache: false,
+          url: url,
+          success: function (data) {
+              console.log(data);
+              $("#recipeTitle").html(data.Title);
+              $("#instructions").html(data.Instructions);
+          }
+      });
   }
-});
+
+  getRecipeJson();
+}
