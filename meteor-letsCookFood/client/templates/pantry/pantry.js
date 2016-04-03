0fe1853cd1789c0e3724288f07fb2c95b899978e
Justin <justinwayneolson@gmail.com>
2016-04-03 02:31:18 -0400

    third endpoint complete

--- meteor-letsCookFood/client/templates/pantry/pantry.js
+++ meteor-letsCookFood/client/templates/pantry/pantry.js
@@ -3,7 +3,6 @@ Template.pantryList.helpers({
       return PantryList.find();
    }
 });
-
 Template.pantryList.events({
       'click .listAdd': function(e) {
          $('#createListAddPopup').modal({
@@ -16,6 +15,20 @@ Template.pantryList.events({
       },
    'click #save': function(e) {
       var data_to_parse = $('#add_to_pantry').val();
+      var array_to_parse = data_to_parse.replace(/ /g, "").split(",");
+       //  $.each(array_to_parse, function(index, value){
+      var ajax=$.ajax({
+                url: 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/parseIngredients', // The URL to the API. You can get this in the API page of the API you intend to consume
+                type: 'POST', // The HTTP Method, can be GET POST PUT DELETE etc
+                data: {'ingredientList':'3 eggs', "servings":"2"}, // Additional parameters here
+                dataType: 'JSON',
+                success: function(data) { console.dir((data.source)); },
+                error: function(err) { alert(err); },
+                beforeSend: function(xhr) {
+                xhr.setRequestHeader("X-Mashape-Authorization", "DVIsMTrR1PmshMNnKC5T7v9XqFbZp1jPadwjsnQMMeyTlkY0pJ"); // Enter here your Mashape key
+             }
+            })();
+        // });
       listItem={};
       Meteor.call('pantryListInsert', listItem, function(error, result){
          return 0;
